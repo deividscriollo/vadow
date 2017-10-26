bf4705207ff55fc56790cdd7c72793fc79fbda1c
David Esteban Criollo <deividscriollo@gmail.com>
2017-10-26 06:39:04 -0500

    Estructura inicial

---
+++ README.md
@@ -0,0 +1,44 @@
+# Vadow Project
+
+Inicio Proyecto gestión administrativa
+
+## Getting Started
+
+Implementación
+
+### Prerequisites
+
+
+```
+- WINDOWS
+
+  XAMPP
+  POSTGRESQL
+  NODEJS
+
+```
+
+### Installing
+
+En la carpeta 
+```
+npm install
+```
+
+### View / Deployment
+
+```
+localhost/vadow
+```
+
+
+## Versioning
+
+1.0.0
+
+## Authors
+
+
+## License
+
+UNLICENSED, este proyecto es privado.
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	 <meta charset="utf-8">
+  <title>Vadow</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+  <link rel="icon" type="app/image/ico" href="fluicon.png">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="./app/styles/main.css">
+</head>
+<body>
+
+	<script src="./node_modules/angular/angular.js"></script>
+</body>
+</html>
